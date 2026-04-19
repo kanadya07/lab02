6f42d494a2a18fd6b9568bbd334aebe553007f19
kanadya07 <kanadya07@mail.ru>
2026-04-19 13:28:17 +0200

    fix code style: remove using namespace std

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,12 +2,9 @@
 #include <string>
 
 int main() {
-    // Prompt user for their name
     std::string name;
     std::cout << "Enter your name: ";
     std::cin >> name;
-    
-    // Print greeting message
     std::cout << "Hello world from " << name << std::endl;
     return 0;
 }
